3d8135ff3754740c98460fe442e90d62af7c4a67
Surojit Saha <sahasurojit06@gmail.com>
2021-03-27 11:39:01 +0530

    Added signup page and working

--- account/views.py
+++ account/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
-# Create your views here.
+def home(request):
+    return render(request, 'product/home.html')
+
+def login(request):
+    return render(request, 'account/login.html')
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1']==request.POST['password2']:
+            try:
+                user=User.objects.get(username=request.POST['username'])
+                return render(request, 'account/signup.html', {'error':'Username has already been taken'})
+            except User.DoesNotExist:
+                user=User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'account/signup.html', {'error':'Password must match'})
+    else:
+        pass
+    return render(request, 'account/signup.html')
+
+def logout(request):
+    #need to go to homepage and logout
+    return render(request, 'account/signup.html')
--- producthuntclone/urls.py
+++ producthuntclone/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from product import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('account/', include('account.urls')),
 ]
