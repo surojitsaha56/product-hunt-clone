28733a10e6b2f1f92f3920b708eb1b1a4e2745cb
Surojit Saha <sahasurojit06@gmail.com>
2021-03-29 13:25:46 +0530

    Added product registration

--- producthuntclone/urls.py
+++ producthuntclone/urls.py
@@ -2,9 +2,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from product import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('account/', include('account.urls')),
-]
+    path('product/', include('product.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
